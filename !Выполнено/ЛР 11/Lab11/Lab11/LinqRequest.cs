2817159db34d9271a0f5ee5b5de9dd37432b8d7b
ankoyeeri <ray.zug@gmail.com>
2020-12-19 22:29:27 +0300

    Lab 11. Done

---
+++ !Выполнено/ЛР 11/Lab11/Lab11/LinqRequest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace Lab11
+{
+    class LinqRequest
+    {
+        /// <summary>
+        /// Покупатели в алфавитном порядке
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        public static void BuyersInAlphabet(List<Buyer> list)
+        {
+            var result = from l in list
+                         orderby l.Name
+                         select l;
+
+            Console.WriteLine("\nВывод в алфавитном порядке:");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+
+        /// <summary>
+        /// Покупатели в определенном диапазоне
+        /// </summary>
+        /// <param name="list"></param>
+        public static void BuyersCardInterval(List<Buyer> list)
+        {
+            int start = 300000;
+            int end = 399999;
+
+            var result = from l in list
+                         where l.CardNumber > start && l.CardNumber < end
+                         select l;
+
+
+            Console.WriteLine($"\nВывод покупателем с диапазоном карты от {start} до {end}");
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Name}    { item.CardNumber}");
+            }
+        }
+
+        /// <summary>
+        /// Максимальный покупатель
+        /// </summary>
+        /// <param name="list"></param>
+        public static void MaxBuyer(List<Buyer> list)
+        {
+            //var result = from l in list
+            //             orderby list.OrderBy(p => p.Balance).FirstOrDefault()
+            //             select l;
+
+            var result = list.Aggregate((i1, i2) => i1.Balance > i2.Balance ? i1 : i2);
+
+            Console.WriteLine($"\nМаксимальный покупатель");
+            Console.WriteLine($"{result.Name}\t\t{result.CardNumber}\t\t{result.Balance}");
+        }
+
+        public static void FiveMaxBuyers(List<Buyer> list)
+        {
+            //var result = list.OrderBy(p => p.Balance).FirstOrDefault();
+            var result = list.Where(p => p.Balance > 0).OrderByDescending(x => x.Balance).Take(5);
+            Console.WriteLine("\nПервые пять покупателей");
+            foreach(var item in result)
+            {
+                Console.WriteLine($"{item.Name}             {item.Balance}");
+            }    
+        }
+    }
+}
--- !Выполнено/ЛР 11/Lab11/Lab11/Program.cs
+++ !Выполнено/ЛР 11/Lab11/Lab11/Program.cs
@@ -10,6 +10,7 @@ namespace Lab11
     {
         static void Main(string[] args)
         {
+            int count = 1;
 
             //  1 Задание
             string[] months = {
@@ -17,7 +18,8 @@ namespace Lab11
                 "October", "November", "December", "January",
                 "February", "March", "April", "May"
             };
-            
+
+            Console.WriteLine($"\nЗадание {count++}");
             //StringFunctionCaller.StringFirstMethod(months);
             //StringFunctionCaller.StringSecondMethod(months);
             //StringFunctionCaller.StringThirdMethod(months);
@@ -37,11 +39,48 @@ namespace Lab11
                 new House {flatNumber = 8, flatArea = 27, flatFloor = 1, flatRoomQuant = 3, flatStreet = "Tesst", flatLifetime = DateTime.Now}
             };
 
+            Console.WriteLine($"\nЗадание {count++}");
             //ListFunctionCaller.ListFirstFunction(houses);
 
 
             //  Задание 3
+            List<Buyer> buyers = new List<Buyer>()
+            {
+                new Buyer { Name = "Михаил",    CardNumber = 794274, Balance = 150  },
+                new Buyer { Name = "Анастасия", CardNumber = 312993, Balance = 954  },
+                new Buyer { Name = "Сергей",    CardNumber = 666228, Balance = 666  },
+                new Buyer { Name = "Вячеслав",  CardNumber = 921801, Balance = 439  },
+                new Buyer { Name = "Виталий",   CardNumber = 394837, Balance = 1000 },
+                new Buyer { Name = "Александра",CardNumber = 794274, Balance = 635  },
+            };
 
+            Console.WriteLine($"\nЗадание {count++}");
+            //LinqRequest.BuyersInAlphabet(buyers);
+            //LinqRequest.BuyersCardInterval(buyers);
+            //LinqRequest.MaxBuyer(buyers);
+            //LinqRequest.FiveMaxBuyers(buyers);
+
+
+            //  Задание 4
+
+            var request = months.Where(p => p.Any(w => p.StartsWith('J'))).OrderByDescending(p => p.Length).Select((p, i) => $"{i++})" + p);
+
+            Console.WriteLine($"\nЗадание {count++}");
+            foreach (var item in request)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+            //Задание 5
+            string[] countries = { "Беларусь", "Литва", "Норвегия", "Швейцария", "Австралия" };
+            int[] key = { 1, 4, 6, 8 };
+            var countrRequest = countries.Join(key, w => w.Length, q => q, (w,q) => new { country = w, id = q });
+
+            Console.WriteLine($"\nЗадание {count++}");
+            foreach (var item in countrRequest)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
